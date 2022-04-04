b69f6281d79c60cd951dbd420ef2d5b2bfdbeee3
TimTheOverlord <timdhuff@gmail.com>
2022-04-04 15:38:11 -0400

    Added Try-Catch handling for bad variable inputs.
    Cleaned up response message to make it more verbose.

--- flask_back_api.py
+++ flask_back_api.py
@@ -122,7 +122,8 @@ def insert():
             response.headers.add("Access-Control-Max-Age", "300")
 
         except KeyError:
-            response = make_response(("Incorrect Payload. Please check that all required payload variables are present.", 400))
+            response = make_response(("Incorrect Payload. Please check that all required payload variables are "
+                                      "present and that their names are correct.", 400)) 
             response.headers.add('Access-Control-Allow-Origin', '*')
             response.headers.add("Access-Control-Allow-Headers", "Content-Type")
             response.headers.add("Access-Control-Allow-Methods", "POST, OPTIONS")
